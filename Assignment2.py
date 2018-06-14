ca132b99a5c697256b36cbe107aa9dd9ba639357
Sarthak1503 <www.sarthak1503@gmail.com>
2018-06-14 12:01:45 +0530

    2nd Assignment added

---
+++ Assignment2.py
@@ -0,0 +1,25 @@
+#1.Print anything you want on Screen
+print(""" Hello! My Name is Sarthak 
+This is my First Python Program""")
+
+#2.Join two strings using '+'
+print("Acad"+"View")
+
+#3.Take the input of 3 variables x,y&z. Print their values on Screen.
+x=input("Enter the value of x=")
+y=input("Enter the value of y=")
+z=input("Enter the value of z=")
+
+#4.Print Let's get started 
+print('"Let\'s get started"')
+
+#5.Placing the values using placeholders
+course="Python"
+Fees=5000
+s="Acadview"
+print('s: %s\nCourse: %s\nFees: %d'% (s,course,Fees))
+
+#6.Find Error and Solve this: print('%s"%d')%(_,_)
+name="Tony Stark"
+salary=1000000
+print('Name: %s, Salary: %d'% (name,salary))
