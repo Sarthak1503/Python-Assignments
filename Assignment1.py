38b95c79c7da820838fcc29450b8612801787feb
Sarthak1503 <www.sarthak1503@gmail.com>
2018-06-14 11:42:43 +0530

    Git commands added

---
+++ Assignment1.py
@@ -0,0 +1,17 @@
+#GIT Commands
+git config --global user.name "Your full name"
+git config --global user.email "Your email"
+git init
+git add filename #for selective approach
+git add . #for all files
+git status #for checking the changes made after or before updation
+git commit -m"your message"
+git log. #shows all the commits that we made
+git remote add origin # Copy the link from github repository
+git push origin master # Tap enter and wait for compiling
+
+#GIT Updations
+git add .
+git commit -m"your message"
+git status
+git push origin master 
